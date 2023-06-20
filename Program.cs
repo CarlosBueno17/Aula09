45b7ea07f1ddc9d2405f29c5e6bf7cbf40c5a934
CarlosBueno17 <135742315+CarlosBueno17@users.noreply.github.com>
2023-06-20 15:07:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int pontos= 0;
+            Console.WriteLine("Bem-vindo ao jogo do 21");
+            Console.WriteLine("Faça 21 pontos para vencer");
+
+            while (true)
+            {
+
+                Console.WriteLine($"Você tem {pontos} pontos ");
+                Console.WriteLine("Digite 1 para continuar ou 2 para parar:");
+                int resposta=int.Parse(Console.ReadLine());
+
+                if(resposta == 2 ) 
+                {
+                    Console.WriteLine("Fim de jogo");
+                    break;
+                    
+                }
+                Random sorteador=new Random();
+                int numero=sorteador.Next(1,11);
+                Console.WriteLine("Você tirou o numero "+numero);
+
+                pontos += numero;
+
+                if (pontos >= 21)
+                {
+                    Console.WriteLine("Fim de jogo!");
+                    break;
+
+                }
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
